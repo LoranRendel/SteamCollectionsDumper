14e5b58bbccf9a4a290efbe4ed1d7122989985f5
Naomi <614231+healingbrew@users.noreply.github.com>
2019-09-19 14:37:17 +0100

    Add files

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using LevelDB;
+using System;
+using System.IO;
+using System.Text;
+
+namespace SteamCollectionPOC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "Steam", "htmlcache", "Local Storage", "leveldb");
+            using (var db = new DB(new Options { CreateIfMissing = false }, root))
+            {
+                foreach (var (key, value) in db)
+                {
+                    var keyText = Encoding.UTF8.GetString(key).Split("\0\u0001");
+                    if (!keyText[0].Equals("_https://steamloopback.host")) continue;
+                    if (!keyText[1].Contains("cloud-storage-namespace")) continue;
+                    File.WriteAllBytes(Path.Combine(root, keyText[1]+".json"), value);
+                }
+            }
+        }
+    }
+}
